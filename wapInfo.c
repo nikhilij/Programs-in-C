2a68dac5be702712ec3807c78b1a720114a6497d
nikhilij <roynikhil973@gmail.com>
2024-08-01 21:24:59 +0530

    First commit

---
+++ wapInfo.c
@@ -0,0 +1,11 @@
+//wap to print your name,date of birth and mobile number
+
+#include<stdio.h>
+
+int main(){
+    printf("Name    : Nikhil Soni");
+    printf("DOB     : Nov 08, 2003");
+    printf("Mobile  : 92633333789");
+
+    return 0;
+}
